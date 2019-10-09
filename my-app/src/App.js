cd6856c1e82482d8fb1241aeabdf1b2fe3034d00
montezakarim <kmonteza@hotmail.com>
2019-10-09 15:53:41 -0500

    login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section class="login column white" id="login">
+    <img class="logo" src="" alt="Logo Burger Queen"/>
+    <div>
+      <div class="login-input column calibri">
+        <label for="email">Correo electrónico:</label>
+        <input class="user" id="email" name="email" type="text"/>
+        <label for="password">Contraseña:</label>
+        <input class="user b-radius4 font16" id="password" name="password" type="password"/>		
+      </div>
+        <div class="login-input column calibri">
+          <p id="message" class="hide center-s"></p>
+          <button class="btn-login b-radius4 font16 white" id="btn-login"><b>INGRESA</b></button>
+        </div>   
     </div>
+  </section>
   );
 }
 
