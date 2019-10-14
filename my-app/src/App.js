c0511821109c806f2535df3782c2633cc52d87d7
gabhu-dev <gabhu@hotmail.es>
2019-10-13 20:58:15 -0500

    diseño de login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,22 +6,21 @@ import React from 'react';
 import Login from './components/login/Login.js';
 import TakeOrders from './components/TakeOrders'
 import {
-BrowserRouter as Router,
+  BrowserRouter as Router,
   Switch,
- Route,
- } from "react-router-dom";
+  Route,
+} from "react-router-dom";
 
 function App() {
 
-  return  <Router>
-            <Switch>
-              <Route path="/takeOrders"> <TakeOrders />
-              </Route>
-              <Route path="/"> <Login/>
-              </Route>
-            </Switch> 
-          </Router>
+  return <Router>
+    <Switch>
+      <Route path="/takeOrders"> <TakeOrders />
+      </Route>
+      <Route path="/"> <Login />
+      </Route>
+    </Switch>
+  </Router>
 }
-
 
 export default App;
