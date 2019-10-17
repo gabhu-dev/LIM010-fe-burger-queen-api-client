7ba783a60a8704787d901fb324fb45086ed179af
gabhu-dev <gabhu@hotmail.es>
2019-10-17 11:34:20 -0500

    añadiendo fetch-mock

--- my-app/src/__test__/Token.spec.js
+++ my-app/src/__test__/Token.spec.js
@@ -1,7 +1,9 @@
-const nock = require('nock');
+const fetchMock = require('../__mocks__/node-fetch')
 import getToken  from '../components/login/Token.js';
   
 describe('getToken function', () => {
+  fetchMock
+  .mock('http://localhost:5000/auth',200)
   it('Have to get token', (done) => {
     getToken('gabhu@hotmail.com','123456')
       .then( res => {
@@ -9,7 +11,6 @@ describe('getToken function', () => {
         expect(res.token).toBe('ok')
         done()
       })
-  })
   it('Have to get token', (done) => {
     getToken()
       .then( res => {
