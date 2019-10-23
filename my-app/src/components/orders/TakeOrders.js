8a7039244e61ce05de9b4f54a4055b3c9389c316
gabhu <gabhu@hotmail.es>
2019-10-23 18:00:34 -0500

    aun no terminado

--- my-app/src/components/orders/TakeOrders.js
+++ my-app/src/components/orders/TakeOrders.js
@@ -1,33 +1,53 @@
 // se unen todos los componentes 
 // -> header,listProducts,
 import React from 'react';
-import getProducts from '../../controller/products/Products' 
-// import showProduct from './ShowProducts';
+import getProducts from '../../controller/products/Products' ;
+import ShowData from '../products/ListProducts';
 const TakeOrders = () => {
+  // const numbers = [1, 2, 3, 4, 5];
+  const products = [
+      {
+        _id:"0001",
+        name:"agua",
+        price:2,
+        image:"http/agua.com",
+        type:"bebidas",
+        dateEntry:"10/10/10"
+      },
+      {
+        _id:"0002",
+        name:"coca-cola",
+        price:4,
+        image:"http/coca.com",
+        type:"bebidas",
+        dateEntry:"10/10/10"
+      },
+      {
+        _id:"0003",
+        name:"hamburguesa",
+        price:8,
+        image:"http/hamburguer.com",
+        type:"hamburguesa",
+        dateEntry:"10/10/10"
+      }
+    ];
+  const listItems = products.map((number) =>
+    <li>{number.type}</li>
+  );
   const handleBreak = (e)=>{
     e.preventDefault()
     const token = localStorage.getItem('token');
-    console.log(token)
+    console.log(token);
     getProducts(token)
-    .then(res=> console.log(res))
+    .then(res=> ShowData(res))
   }
-  // const [userId, setUserId] = useState("");
-  // const [client, setClient] = useState("");
-  // const [products, setProducts] = useState("");
-  // const [productId, setProductId] = useState("");
-
-  // const handleClientChange = (e) =>{
-  //   setClient(e.target.value);
-  // }
-  // const handleProduct = (e) =>{
-  //   getSpecificProduct()
-  // }
-  // llamar a todos los producos y pintarlos
   return (
   <div> 
     <button
     onClick= {handleBreak}>desayuno</button>
-    <button>almuerzo</button>
+    <button >almuerzo</button>
+    <textarea></textarea>
+    <ul>{listItems}</ul>
   </div>
   );
 };
--- my-app/src/components/products/ListProducts.js
+++ my-app/src/components/products/ListProducts.js
@@ -1,5 +1,38 @@
 // componente de los productos
 // lo que se pinta
-const product = (data)=>{
-
+// import React from 'react';
+const ShowData = (data)=>{
+console.log(data);
+// const products = [
+//   {
+//     _id:"0001",
+//     name:"agua",
+//     price:2,
+//     image:"http/agua.com",
+//     type:"bebidas",
+//     dateEntry:"10/10/10"
+//   },
+//   {
+//     _id:"0002",
+//     name:"coca-cola",
+//     price:4,
+//     image:"http/coca.com",
+//     type:"bebidas",
+//     dateEntry:"10/10/10"
+//   },
+//   {
+//     _id:"0003",
+//     name:"hamburguesa",
+//     price:8,
+//     image:"http/hamburguer.com",
+//     type:"hamburguesa",
+//     dateEntry:"10/10/10"
+//   }
+// ];
+// const number=[1,2,3,4];
+// const ListProducts = number.map((obj)=><li>{obj}</li>);
+// return(
+//  <ul>{ListProducts}</ul> 
+// )
 }
+export default ShowData;
