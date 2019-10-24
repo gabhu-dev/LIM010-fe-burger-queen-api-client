f1513fa51c4b2d9cbb46d3bae329fabf9f06dc27
gabhu-dev <gabhu@hotmail.es>
2019-10-24 11:49:34 -0500

    pintando productos

--- my-app/src/components/orders/TakeOrders.js
+++ my-app/src/components/orders/TakeOrders.js
@@ -1,54 +1,28 @@
 // se unen todos los componentes 
 // -> header,listProducts,
-import React from 'react';
-import getProducts from '../../controller/products/Products' ;
-import ShowData from '../products/ListProducts';
-const TakeOrders = () => {
-  // const numbers = [1, 2, 3, 4, 5];
-  const products = [
-      {
-        _id:"0001",
-        name:"agua",
-        price:2,
-        image:"http/agua.com",
-        type:"bebidas",
-        dateEntry:"10/10/10"
-      },
-      {
-        _id:"0002",
-        name:"coca-cola",
-        price:4,
-        image:"http/coca.com",
-        type:"bebidas",
-        dateEntry:"10/10/10"
-      },
-      {
-        _id:"0003",
-        name:"hamburguesa",
-        price:8,
-        image:"http/hamburguer.com",
-        type:"hamburguesa",
-        dateEntry:"10/10/10"
-      }
-    ];
-  const listItems = products.map((number) =>
-    <li>{number.type}</li>
-  );
+import React,{useState} from 'react';
+import getProducts from '../../controller/products/products';
+import ListProducts from '../products/ListProducts';
+const TakeOrders = (props) => {
+  const [productData, setProductData]=useState([]);
   const handleBreak = (e)=>{
     e.preventDefault()
     const token = localStorage.getItem('token');
-    console.log(token);
     getProducts(token)
-    .then(res=> ShowData(res))
-  }
-  return (
-  <div> 
-    <button
-    onClick= {handleBreak}>desayuno</button>
-    <button >almuerzo</button>
-    <textarea></textarea>
-    <ul>{listItems}</ul>
+    .then(res=> setProductData(res))
+}
+return(
+  <main>
+  <button
+    onClick= {handleBreak}
+  >
+  desayuno
+  </button>
+  <button>almuerzo</button>
+  <div>
+    <ListProducts data={productData}/>
   </div>
-  );
+</main>
+)
 };
 export default TakeOrders;
--- my-app/src/components/products/ListProducts.js
+++ my-app/src/components/products/ListProducts.js
@@ -1,38 +1,17 @@
-// componente de los productos
-// lo que se pinta
-// import React from 'react';
-const ShowData = (data)=>{
-console.log(data);
-// const products = [
-//   {
-//     _id:"0001",
-//     name:"agua",
-//     price:2,
-//     image:"http/agua.com",
-//     type:"bebidas",
-//     dateEntry:"10/10/10"
-//   },
-//   {
-//     _id:"0002",
-//     name:"coca-cola",
-//     price:4,
-//     image:"http/coca.com",
-//     type:"bebidas",
-//     dateEntry:"10/10/10"
-//   },
-//   {
-//     _id:"0003",
-//     name:"hamburguesa",
-//     price:8,
-//     image:"http/hamburguer.com",
-//     type:"hamburguesa",
-//     dateEntry:"10/10/10"
-//   }
-// ];
-// const number=[1,2,3,4];
-// const ListProducts = number.map((obj)=><li>{obj}</li>);
-// return(
-//  <ul>{ListProducts}</ul> 
-// )
-}
-export default ShowData;
+import React from 'react';
+
+const  ListProducts = ({data}) => {
+ return data.map(product => (
+  <div  key={product._id} >
+    <img src={product.image} alt={product.name} />
+    <div>
+      <h5>{product.name}</h5>
+    </div>
+    <div>
+      S/. {product.price}
+    </div>
+  </div>
+ ))
+};
+
+export default ListProducts;
