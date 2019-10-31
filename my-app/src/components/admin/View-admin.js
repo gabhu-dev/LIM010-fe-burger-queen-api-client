86ebef968aaff05ec67facbe9da6d42d4398e340
gabhu <gabhu@hotmail.es>
2019-10-31 17:57:36 -0500

    agregar function

--- my-app/src/components/admin/View-admin.js
+++ my-app/src/components/admin/View-admin.js
@@ -2,8 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Header from '../orders/Header';
 import getUsers from '../../controller/users/users';
 import ListUsers from '../users/List-users';
+import addUsers from '../../controller/users/add-user';
+
 const ViewAdmin = (props) =>{
   const [usersData, setUsersData] = useState([]);
+  // const [show, setShow] = useState(true);
+  // const [user, setuser] = useState("");
+
+  // const updateUser = (e)=>{
+  //   setuser(e.target.value);
+  // }
+  // const handleCreateUsers = (e)=>{
+  //   e.preventDefault();
+  //   addUsers()
+  // }
+  // const onSubmit = (e) => {
+  //   e.preventDefault(); 
+  //   setShow(show === true ? false : true)
+  // }
   useEffect(()=>{
     const token = localStorage.getItem('token');
     getUsers(token)
@@ -14,8 +30,16 @@ const ViewAdmin = (props) =>{
     <div>
       <Header/>
       <table>
+      < tbody > 
+      <tr>
+        <td>Email</td>
+        <td>Editar</td>
+        <td>Eliminar</td>
+      </tr>
       <ListUsers data={usersData}/>
+      </ tbody > 
       </table>
+      <button>Agregar usuario</button>
     </div>
   )
 }
--- my-app/src/components/users/List-users.js
+++ my-app/src/components/users/List-users.js
@@ -1,12 +1,35 @@
 import React from 'react';
 // import { Table } from 'semantic-ui-react';
-const  ListUsers = ({data}) => { 
-  return data.map(users => (
+// const  ListUsers = ({data, user ,updateUser , show, setShow}) => { 
+// // return (
+//     {show === true && (
+//      data.map(users => (
+//         <tr className="hu" key={users._id} >
+//             <td>{users.email}</td>  
+//             <td><i className="far fa-edit"></i></td>    
+//             <td><i className="far fa-trash-alt"></i></td>      
+//       </tr>
+//        ))
+//     )}
+//     {show === false && (
+//       <button>egegegeg</button>
+//     )}
+import deleteUsers from '../../controller/users/delete-user';
+const ListUsers= ({data}) => {
+  const handleDeleteUser = (e) =>{
+    e.preventDefault();
+    deleteUsers(localStorage.getItem('token'),data[0]._id)
+    .then((response)=>{
+      console.log(response);
+    })
+    console.log(data[0]._id)
+  }
+return data.map(users => (
   <tr className="hu" key={users._id} >
     {/* <h4>{users.email}</h4> */}
         <td>{users.email}</td>  
-        <td>editar</td>    
-        <td>eliminar</td>      
+        <td><button onClick = {handleDeleteUser}><i className="far fa-edit"></i></button></td>    
+        <td><i className="far fa-trash-alt"></i></td>      
   </tr>
  ))
 };
